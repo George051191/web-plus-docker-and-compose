5f0de9df81e14e056f0f4b79bc20aa45da28df74
George051191 <trubacheff_91@mail.ru>
2022-10-20 09:45:36 +0300

    docker file added

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'app1',
+      script: './main.js',
+      env_production: {
+        JWT_SECRET: 'df56e3397961d9f44008e62ae5ef6d84',
+        POSTGRES_PASSWORD: 'student',
+        POSTGRES_DB: 'kupipodariday',
+        POSTGRES_USER: 'student',
+        POSTGRES_HOST: 'localhost',
+        POSTGRES_PGDATA: '/var/lib/postgresql/data/pgdata',
+      },
+    },
+  ],
+};
